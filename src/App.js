4651d90041aafb1ad434ffd49f3816641290a218
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-01-05 17:07:18 +0100

    add App Styles

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+            <h1><span>NOT</span><span>todo</span><span>LIST</span></h1>
+      <ol id="instructions">
+        <li>Write down things not to do.</li>
+        <li>
+          If someone asks you to do said thing, just reply with: "Sorry, but
+          that's on my Not Todo List."
+        </li>
+        <li>
+          If you successfully avoided doing said thing, cross it of your list.
+        </li>
+      </ol>
+      {/* The form component goes here */}
+      {/* <Form /> */}
+      <i className="license">icons by fontawesome -  <a href="https://fontawesome.com/license">license</a></i>
     </div>
   );
 }

68add7c61be8dee7d6ca5987070b63de45bbfaf6
Stefan Klarmann <klarmann1@googlemail.com>
2021-01-06 13:39:20 +0100

    added files/structure/style for edit-, delete-, todo-component

--- src/ToDoList/index.js
+++ src/ToDoList/index.js
@@ -1,11 +1,14 @@
 import React from "react";
+import ToDoItem from "../ToDoItem";
 import "./styles.css";
 
 export default function ToDoList() {
   return (
     <div>
       <p>Things not to do:</p>
-      <ul id="task-list"></ul>
+      <ul id="task-list">
+        <ToDoItem />
+      </ul>
     </div>
   );
 }

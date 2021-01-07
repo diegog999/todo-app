77a825cb4f6275892f317bebb5888fe59751179f
diegog999 <42815611+diegog999@users.noreply.github.com>
2021-01-07 22:05:35 +0100

    delete function

--- src/ToDoItem/index.js
+++ src/ToDoItem/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./styles.css";
 
-export default function ToDoItem() {
+export default function ToDoItem({ todoList, handleDelete }) {
   return (
-    <li id="">
+    <li id="1">
       <span class="text">Skip morning coffee</span>
       <span className="bttn edit"></span>
-      <span className="bttn delete"></span>
+      <span
+        className="bttn delete"
+        onClick={(e) => {
+          console.log(e);
+          handleDelete(e);
+        }}
+      ></span>
     </li>
   );
 }
--- src/ToDoList/index.js
+++ src/ToDoList/index.js
@@ -2,12 +2,34 @@ import React from "react";
 import ToDoItem from "../ToDoItem";
 import "./styles.css";
 
-export default function ToDoList() {
+export default function ToDoList({ toDoList, setToDoList }) {
+  const handleDelete = (e) => {
+    const todoId = e.target.parentNode.id; //find index from List
+    console.log(todoId);
+    console.log(toDoList);
+    const newArr = toDoList;
+    toDoList.map((todo, index) => {
+      if (todo.id === todoId) {
+        return newArr.splice(index, 1);
+      }
+    });
+
+    console.log(newArr);
+    setToDoList(newArr); //delete element from List
+    console.log(toDoList);
+  };
+
+  // this.setState((prevState) => {
+  //   const components = prevState.components.slice(0);
+  //   components.splice(0, 0, element);
+  //   return { components };
+  // });
+
   return (
     <div>
       <p>Things not to do:</p>
       <ul id="task-list">
-        <ToDoItem />
+        <ToDoItem handleDelete={handleDelete} />
       </ul>
     </div>
   );

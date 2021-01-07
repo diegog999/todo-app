21913542e8ef8b397012e86187718410f9f5531e
Stefan Klarmann <klarmann1@googlemail.com>
2021-01-07 18:47:28 +0100

    added mark as done function to todo-item

--- src/ToDoItem/index.js
+++ src/ToDoItem/index.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function ToDoItem() {
+  const [textClass, setTextClass] = useState("");
+
+  const checkedHandler = () => {
+    if (textClass === "") {
+      setTextClass("checked");
+    } else {
+      setTextClass("");
+    }
+  };
+  const editHandler = () => {
+    console.log("hello");
+  };
+
   return (
-    <li id="">
-      <span class="text">Skip morning coffee</span>
-      <span className="bttn edit"></span>
+    <li className={textClass}>
+      <span onClick={checkedHandler} className="text">
+        Skip morning coffee
+      </span>
+      <span className="bttn edit" onClick={editHandler}></span>
       <span className="bttn delete"></span>
     </li>
   );

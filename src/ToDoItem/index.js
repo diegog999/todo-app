451b5e2046da39ecadde2de1f482d1531f57a13f
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-01-08 15:32:53 +0100

    fix className

--- src/ToDoItem/index.js
+++ src/ToDoItem/index.js
@@ -4,12 +4,11 @@ import "./styles.css";
 export default function ToDoItem({ toDo, handleDelete }) {
   return (
     <li key={toDo.id}>
-      <span class="text">{toDo.title}</span>
+      <span className="text">{toDo.title}</span>
       <span className="bttn edit"></span>
       <span
         className="bttn delete"
         onClick={(e) => {
-          console.log(e);
           handleDelete(e);
         }}
       ></span>
